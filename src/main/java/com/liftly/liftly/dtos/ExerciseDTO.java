c49e616e4aac500c27f6db9605bea732b00070a7
Adri-Casas <acasas@sercide.com>
2025-11-16 12:21:15 +0100

    (Feat): Return exercise ID when creating new one

--- src/main/java/com/liftly/liftly/dtos/ExerciseDTO.java
+++ src/main/java/com/liftly/liftly/dtos/ExerciseDTO.java
@@ -4,9 +4,11 @@ package com.liftly.liftly.dtos;
 import lombok.Data;
 
 import java.util.List;
+import java.util.Optional;
 
 @Data
 public class ExerciseDTO {
+    private Optional<Integer> id;
     private String name;
     private List<WorkoutSetDTO> sets;
 }
